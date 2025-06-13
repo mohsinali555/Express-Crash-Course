08564cc27830c55963cfed4fe2f3c8ffe9c230e6
Subhani55 <m.mohsinalisubhani5@gmail.com>
2025-06-13 10:46:45 +0500

    Last Commit

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 import express from "express";
 import path from "path";
+import { fileURLToPath } from "url";
 import posts from "./routes/posts.js";
 import logger from "./middleware/logger.js";
 import errorHandler from "./middleware/error.js";
+import notFound from "./middleware/notFound.js";
 const port = process.env.PORT || 8000;
+
+// Get the directory name
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -15,12 +21,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(logger);
 
 // setup static folder
-//app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 //Routes
 app.use("/api/posts", posts);
 
 // Error Handler
+app.use(notFound);
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
